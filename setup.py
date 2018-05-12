c402b2eaf8cd4bdec3088ae82879174ce14c7c52
Seokhwan Cheon <chaos314@gmail.com>
2018-05-12 19:53:56 +0900

    Updated description

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ dependencies = [
 
 version = '1.0.0'
 
-setup(name='aio-upbit',
+setup(name='aioupbit',
       version=version,
       packages=['aioupbit'],
-      description='Python wrapper for the Upbit API',
+      description='Asynchronous wrapper for the Upbit API',
       url='https://github.com/chaos314/aio-upbit',
       author='Seokhwan Cheon',
       author_email='chaos314@gmail.com',
@@ -21,10 +21,7 @@ setup(name='aio-upbit',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
-          'Development Status :: 3 - Alpha',
           'Topic :: Office/Business :: Financial',
       ],
 )
